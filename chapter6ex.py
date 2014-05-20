ba125e9b57b4a093465fa9f116c660324ba93b3d
Tsung Hung <hungmasterj@gmail.com>
2014-05-20 08:23:52 -0400

    updated

--- chapter6ex.py
+++ chapter6ex.py
@@ -13,3 +13,10 @@ print food[0:2]
 breakfast = 'cookies, cookies, cookies'
 breakfastBrokendown = breakfast.split(', ')
 print breakfastBrokendown
+
+#6.2 Exercises
+cardinal_nums = ('first','second','third')
+print cardinal_nums[1]
+pos1, pos2, pos3 = cardinal_nums #tulip unpacking
+print pos1, pos2, pos3
+#6.3 Exercises
