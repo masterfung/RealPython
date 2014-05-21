a6bfb3d541696bc842aaa31e9cd9cf3e0abb3503
Tsung Hung <hungmasterj@gmail.com>
2014-05-21 14:53:29 -0400

    first
    exit()

---
+++ poetry.py
@@ -0,0 +1,7 @@
+array = [
+Nouns: "fossil", "horse", "aardvark", "judge", "chef", "mango", "extrovert", "gorilla", "chimp", "superhero"
+Verbs: "kicks", "jingles", "bounces", "slurps", "meows", "explodes", "curdles", "hurdles", "jumps"
+Adjectives: "furry", "balding", "incredulous", "fragrant", "exuberant", "glistening", "tasty", "sensual"
+Prepositions: "against", "after", "into", "beneath", "upon", "for", "in", "like", "over", "within", "without", "notwithstanding"
+Adverbs: "curiously", "extravagantly", "tantalizingly", "furiously", "sensuously", "joyously", "religiously"
+]
