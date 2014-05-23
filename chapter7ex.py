dc401ae2a40a9bacc78a795f4f60f51d70b5a437
Tsung Hung <hungmasterj@gmail.com>
2014-05-23 01:11:22 -0400

    more awesome updates

--- chapter7ex.py
+++ chapter7ex.py
@@ -1,4 +1,17 @@
 #7.1 Exercises
+firstInput = open('Practice Files/Chapter 7/Practice files/poem.txt', 'r')
+subject = firstInput.readline()
+while subject != "":
+	print subject,
+	subject = firstInput.readline()
+firstInput.close()
+
 with open('Practice Files/Chapter 7/Practice files/poem.txt', 'r') as myInput, open('Practice Files/Chapter 7/Practice files/test.txt', 'w') as myOutput:
 	for line in myInput.readline():
 		myOutput.write(line)
+
+
+somethingFun = open('Practice Files/Chapter 7/Practice files/test.txt', 'a')
+somethingAmazing = ["I saw the newest X-Men movie on release night and I still love the X-Men! Go see it!"]
+somethingFun.writelines(somethingAmazing)
+somethingFun.close()
