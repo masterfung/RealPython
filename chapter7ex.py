6a6aadaa624b420d75335c6c9d1a3f0ebd2c9237
Tsung Hung <hungmasterj@gmail.com>
2014-05-23 01:04:45 -0400

    more updates

--- chapter7ex.py
+++ chapter7ex.py
@@ -1,4 +1,4 @@
-#7 Exercises
+#7.1 Exercises
 with open('Practice Files/Chapter 7/Practice files/poem.txt', 'r') as myInput, open('Practice Files/Chapter 7/Practice files/test.txt', 'w') as myOutput:
 	for line in myInput.readline():
 		myOutput.write(line)
