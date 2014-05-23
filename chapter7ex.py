61eb74f374ad493fbfd9e20d9ef86700c5f76699
Tsung Hung <hungmasterj@gmail.com>
2014-05-23 01:04:27 -0400

    updating file

---
+++ chapter7ex.py
@@ -0,0 +1,4 @@
+#7 Exercises
+with open('Practice Files/Chapter 7/Practice files/poem.txt', 'r') as myInput, open('Practice Files/Chapter 7/Practice files/test.txt', 'w') as myOutput:
+	for line in myInput.readline():
+		myOutput.write(line)
